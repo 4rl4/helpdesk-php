e0e9557419ac81e32b77993dcc195819519f31bd
Arla Berlianty <arlaberlianty888gmail.com>
2026-04-15 10:32:27 +0700

    Feat: Fitur simpan laporan ke database

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+include'koneksi.php'; //Mengambil kabel koneksi
+
 // cek apakah tombol kirim sudah ditekan
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = $_POST["nama"];
     $laporan = $_POST["laporan"];
-    
-    echo"<div style='color:green;'><b>Sukses!</b>Laporan dari<b>$nama</b> telah diterima:<i>$laporan</i></div><hr>";
+
+    //perintah untuk memasukkan data ke database 
+    $query = "INSERT INTO pengaduan (nama, laporan) VALUES ('$nama', '$laporan')";
+
+    if(mysqli_query($conn,$query)){
+            echo"<div style='color:green;'><b>Sukses!</b>Laporan Berhasil disimpan ke database.</div><hr>"; // jika data berhasil masuk ke database
+    }else {
+        echo "Error:". mysqli_error($conn); // jika data tidak berhasil masuk ke database
+    }
 }
 ?>
 <!DOCTYPE html>
